3ae0560ee5c9f9e05a224e5e200d85efa860ca23
rwzimmerman04 <rwzimmerman04@gmail.com>
2026-06-11 14:01:32 -0700

    feat: add DetectionRule interface, LargePasteRule, and RuleEngine

---
+++ _jetbrains/bin/main/com/alethia/detection/rules/DetectionRule.kt
@@ -0,0 +1,14 @@
+package com.alethia.detection.rules
+
+import com.alethia.detection.events.DetectionEvent
+
+/**
+ * An interface that all teh detection rules implement.
+ * Each rule evaluates a DetectionEvent and returns a rationale string
+ * if the event should be flagged, or null if it should be ignored.
+ * Adding a new rule is just implementing this interface and registering
+ * it in the RuleEngine.
+ */
+interface DetectionRule {
+    fun evaluate(event: DetectionEvent): String?
+}
