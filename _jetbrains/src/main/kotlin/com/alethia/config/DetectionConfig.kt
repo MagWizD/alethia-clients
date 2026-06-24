b55f97a75dcc4372ee87c2c0706690b21af490cb
rwzimmerman04 <rwzimmerman04@gmail.com>
2026-06-24 13:41:33 -0700

    docs: add missing comments for future reference and code review

--- _jetbrains/src/main/kotlin/com/alethia/config/DetectionConfig.kt
+++ _jetbrains/src/main/kotlin/com/alethia/config/DetectionConfig.kt
@@ -1,11 +1,15 @@
 package com.alethia.config
 
 /**
- *
+ * Data class holding all thresholds set by the repo maintainer.
+ * The DetectionConfig is reference by the RuleEngine to evaulate whether
+ * the maintainer wants this information requested.
  */
-
 data class DetectionConfig(
+    // Minimum number of chars to be considered a large insertion
     val largePasteThreshold: Int = 200,
+    // Milliseconds to ignore incoming events, prevents same paste event being caught multiple times
     val debounceWindowMs: Long = 2000,
+    // File patterns to ignore
     val ignorePatterns: List<String> = listOf(".git")
 )
