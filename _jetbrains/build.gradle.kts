581bb3ed9106410fd9d5b0f047152a3527d6642b
rwzimmerman04 <rwzimmerman04@gmail.com>
2026-08-09 13:30:54 -0700

    feat: add JaCoCo to Gradle plugins.
    - jacoco plugin added for generating code coverage test reports after unit tests complete
    - clean up and add additional comments for PR

--- _jetbrains/build.gradle.kts
+++ _jetbrains/build.gradle.kts
@@ -1,20 +1,55 @@
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.gradle.testing.jacoco.tasks.JacocoReport
+import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
 
-// Plugins needed for Gradle to build the project
-// NOT plugins for Alethia's use
+
+// Gradle build plugins: These are tools that help Gradle build the project
+// They are NOT IntelliJ plugins or Alethia plugins
 plugins {
+    // Kotlin JVM complier: this is what elts us compile Kotlin source files to JVM bytecode
     id("org.jetbrains.kotlin.jvm")
+    // IntelliJ Platform Gradle Plugin: Handles sandbox IDE download, runIde, and build tasks
     id("org.jetbrains.intellij.platform")
+    // Changelog pluglin
     id("org.jetbrains.changelog")
+    // JaCoCo: Java Code Coverage library, generates coverage reports from test runs
+    jacoco
 }
 
 dependencies {
+    // JUnit 4: Test framework used for all Alethia Unit Tests
     testImplementation("junit:junit:4.13.2")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension
+    // Defines which IDE to download for the sandbox
+    // and what bundled plugins are required at runtime.
     intellijPlatform {
+        // IDE version (e. Target is IntelliJ IDEA Community 2025.1)
         intellijIdeaCommunity("2025.1")
+        // IntelliJ Platform test framework: required for repo BasePlatformTestCase
         testFramework(TestFrameworkType.Platform)
+        // Git4Idea: bundled git plugin, required fro repo detection, push/commit listeners
         bundledPlugin("Git4Idea")
+    }
+}
+
+// Configure the test task to generate JaCoCo coverage reports after tests are completed
+tasks.named<Test>("test") {
+    // Automatically run the jacocoTestReport task
+    finalizedBy(tasks.named("jacocoTestReport"))
+    extensions.configure<JacocoTaskExtension> {
+        isIncludeNoLocationClasses = true
+        excludes = listOf("jdk.internal.*")
+    }
+}
+
+// Configure the JaCoCo test report
+// Create an HTML coverage report at build/reports/jacoco/test/html/index.html
+tasks.named<JacocoReport>("jacocoTestReport") {
+    // Ensure tests always run before report is generated
+    dependsOn(tasks.named("test"))
+    reports {
+        // Generate an HTML report
+        html.required.set(true)
     }
 }
